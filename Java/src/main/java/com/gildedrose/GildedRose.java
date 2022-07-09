3b77e3a088d2048303b15f0d676d7703fbf552e7
PhongPT <phong.pham@mafc.com.vn>
2022-07-09 20:09:10 +0700

    apply Factory and improve classification Rose by key word in name.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,24 +7,10 @@ class GildedRose {
     }
 
     public void updateQuality() {
+    	ItemFactory itemFactory = new ItemFactory();
+    	IItem item;
     	for (int i = 0; i < items.length; i++) {
-    		IItem item;
-    		switch (items[i].name) {
-            case  "Aged Brie":
-            	item = new AgedBrieItem(items[i].name, items[i].sellIn, items[i].quality);
-                break;
-            case  "Backstage passes to a TAFKAL80ETC concert":
-            	item = new BackstagePassesItem(items[i].name, items[i].sellIn, items[i].quality);
-                break;
-            case  "Sulfuras, Hand of Ragnaros":
-            	item = new SulfurasItem(items[i].name, items[i].sellIn, items[i].quality);
-                break;
-            case  "Conjured Mana Cake":
-            	item = new ConjuredItem(items[i].name, items[i].sellIn, items[i].quality);
-                break;
-            default:
-            	item = new NormalItem(items[i].name, items[i].sellIn, items[i].quality);
-    		}
+    		item = itemFactory.getItem(items[i]);
     		item.updateQuality();
         	this.items[i] = (Item) item;
         }
